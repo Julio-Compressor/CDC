49bf5ffd60921af4fcba0fed52e76dcc12b2170b
julien leblanc <julien2.leblanc@gmail.com>
2024-11-13 14:20:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CDC',
+  webDir: 'www'
+};
+
+export default config;
